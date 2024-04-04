d00c78b3e69c4a7893b39e1a152bd79d2aec0619
ANSHIKA <142472970+Anshika852@users.noreply.github.com>
2024-04-04 21:17:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,113 @@
+// function fun(){
+//     for(var i=0;i<5;i++){
+//         console.log(i);
+//     }
+//     console.log(i);
+
+// }
+// fun();
+// let message="else";
+// console.msg(message);
+
+// let arr=['a','b','c','d'];
+
+// arr[0] = 'abcd';
+// console.log(arr);
+
+
+// let str='12';
+// let age =20;
+
+// let ans= str+age;
+// let ans1= str-age;
+// console.log(ans);
+// console.log(ans1);
+
+
+
+  
+// let a='12';
+// let b =12;
+// if(a==b)   // used to check only for value 
+// {
+//     console.log("yes");
+// }
+// else{
+//     console.log("no");
+
+// }
+
+
+// if(a===b)  //used to check both  data type and value 
+// {
+//     console.log("yes");
+// }
+
+// else{
+//     console.log("no");
+// }
+
+
+// const str3 = 'Anshika';
+// const str4 =' my name is anshika ';
+// //const ans3= `${str3} ${str4}`;  //`` backtik use hta h concatinate krne k liye 
+// const ans3 = str3+str4;
+// console.log(ans3);
+// const f ='just'
+// const c= 'looking';
+// const d = 'like';
+// const e='a wow';
+// const ans4 = ` ${f} ${c} ${d} ${e}`;
+// console.log(ans4);
+
+// function print(str){
+//   console.log(str);
+// }
+
+
+
+// print("hello");
+
+
+
+function sum5(a,b){
+    if(a && b)
+    {
+        console.log(a+b);
+
+    }
+    else{
+        console.log(a);
+    }
+    console.log(sum5);
+
+
+}
+let g =2;
+switch(g){
+    case 1 :
+        console.log('one');
+        break;
+
+     case 2 :
+        console.log('two');
+        break; 
+}
+
+
+const obj={
+    1:'one',
+    'name': 'anshika' ,
+}
+console.log(obj);
+
+// let abc=prompt('my name is anshika ');
+// console.log(abc);
+
+let c = prompt('just looking like a wow ');
+console.log(c);
+
+
+
+
+
